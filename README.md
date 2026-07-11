8305041f07bcba966c71eec2733437d917382c0e
mr-rinshad <mrmuhammedrinshad@gmail.com>
2026-07-11 10:06:50 +0530

    Phase 1 completed - Project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+Chemical Purchase Authorization and Laboratory Management System
+
+MCA Mini Project
+
+Technology Stack
+
+HTML
+CSS
+Bootstrap
+JavaScript
+Node.js
+Express.js
+MySQL
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.send("Chemical Purchase Authorization and Laboratory Management System is Running...");
